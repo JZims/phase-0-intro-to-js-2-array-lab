dffc7ff33afb71e20fac7bc0f0ba7e72673181c5
JZims <JZimmsGames@gmail.com>
2021-03-04 15:36:17 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+  }
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+
+  function appendCat(name) {
+    const copyOfCats = [...cats, "Broom"];
+    return copyOfCats;
+  }
+
+  function prependCat(name) {
+    const copyOfCats = ["Arnold", ...cats];
+    return copyOfCats;
+  }
+
+  function removeLastCat() {
+      const copyOfCats = cats.slice(0, 2);
+      return copyOfCats;
+  }
+
+  function removeFirstCat() {
+    const copyOfCats = cats.slice(1, 3);
+    return copyOfCats;
+}
